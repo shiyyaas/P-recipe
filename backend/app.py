ae97fa422a3cdf6708281a0db8f960658ba17f9f
shiyas <ozil@gmail.com>
2025-09-29 14:23:55 +0530

    Time out

--- backend/app.py
+++ backend/app.py
@@ -1,1 +1,13 @@
-import flask as Flask
+from flask import Flask
+from flask_cors import CORS  
+from pymongo import MongoClient
+
+app = Flask(__name__)
+CORS(app)  # this allows the communications btw frontend and backend
+
+@app.route('/')
+def hello():
+    return "Hello World! Flask is working! 🎉"
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5000)
